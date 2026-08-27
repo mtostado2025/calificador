732165bca1a219e383b0286c8783e87dda7f0c33
mtostado2025 <mariotostado54@gmail.com>
2026-08-27 03:54:44 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+// Service Worker para Calificador Kiosk
+const CACHE_NAME = 'calificador-cache-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './index.css',
+  './app.js',
+  './config.js',
+  './qrcode.js',
+  './vendor_supabase.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './logo.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request).catch(() => caches.match('./index.html'));
+    })
+  );
+});
